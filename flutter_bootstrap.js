df9fee518bf0e9cd4e4e5b9e1625d2ee7d5b9869
Rafael <rafaeljanetzko@gmail.com>
2026-09-22 23:51:15 +0200

    Add join by group name and optional password feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2643639355" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3153471574" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
